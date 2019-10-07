cd89481ce0deb6422fab7196ff181961d4e69965
Rivaldz <rivalzsetyo@gmail.com>
2019-10-07 15:10:56 +0700

    fix error

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/FoodFragment.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/FoodFragment.java
@@ -41,16 +41,16 @@ public class FoodFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
